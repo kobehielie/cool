0f37b5825365b6131d696b41289a1504d58a065a
kobehi elie elisee <edencanaanjes@gmail.com>
2024-10-22 14:35:39 +0000

    first commit

---
+++ fichier.js
@@ -0,0 +1,5 @@
+function Salut() {
+    let nom="kobehi"
+    return nom
+   }
+   console.log("bonjour ".Salut())
---
+++ fichier.py
@@ -0,0 +1,5 @@
+def salut():
+    nom="kobehi"
+    return nom
+
+print(f"bonjour {salut()}")
